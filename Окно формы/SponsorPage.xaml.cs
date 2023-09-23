c2674229e290c832729730dd774772c8ed535364
User <User@WIN-23AB664D43J>
2023-09-23 20:46:41 +0300

    Добавьте файлы проекта.

---
+++ Окно формы/SponsorPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Окно_формы
+{
+    /// <summary>
+    /// Логика взаимодействия для SponsorPage.xaml
+    /// </summary>
+    public partial class SponsorPage : Page
+    {
+        public SponsorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
